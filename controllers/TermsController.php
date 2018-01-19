eae35ae9180bb55ff3b1a57135ac940eab02c6d4
Fly <874847721@qq.com>
2018-01-19 09:51:03 +0800

    v1.0

---
+++ controllers/TermsController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DELL-12
+ * Date: 2018/1/18
+ * Time: 18:07
+ */
+namespace app\controllers;
+
+use yii\web\Controller;
+
+class TermsController extends Controller
+{
+    public function actionIndex(){
+        echo 'index';
+    }
+
+    public function actionView(){
+        echo 'view';
+    }
+
+    public function actionUpdate(){
+        echo 'update';
+    }
+
+    public function actionDelete(){
+        echo 'delete';
+    }
+}
